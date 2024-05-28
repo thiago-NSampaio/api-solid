30ea2fca84ca05ea622f573742e0e37a08aad07d
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-05-28 17:34:50 -0400

    controller Profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -4,25 +4,13 @@ import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-
 import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 
 export async function profile(req:FastifyRequest,reply:FastifyReply){
-    await req.jwtVerify()
-    // const authenticateBodySchema = z.object({
-    //     email: z.string().email(),
-    //     password: z.string().min(6)
-    // })
+    // await req.jwtVerify()
+ 
+    const getUserProfile = makeGetUserProfileUseCase()
 
-    // const { email, password} = authenticateBodySchema.parse(req.body)
-
-    // try {
-    //     const authenticateUseCase = makeGetUserProfileUseCase()
-        
-    //     await authenticateUseCase.execute({email,password})
-    // } catch (err) {
-    //     if(err instanceof InvalidCredentialsError){
-    //         return reply.status(400).send({message:err.message})
-    //     }
-
-    //     throw err
-    // }
+    const {user} = await getUserProfile.execute({
+        userId: req.user.sub
+    })
   
-    return reply.status(200).send()
+    return reply.status(200).send(user)
 }
