5a794c46054e33b734642d37508951bfda5f3d5c
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-05-29 21:26:02 -0400

    prisma environment test

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import { app } from "./app";
 import { env } from "./env";
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config"
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-prisma.user.create({
-  data: {
-    name: "thiago",
-    email: "thiago@email",
-  },
-});
+// const prisma = new PrismaClient();
+// prisma.user.create({
+//   data: {
+//     name: "thiago",
+//     email: "thiago@email",
+//   },
+// });
 app
   .listen({
     host: "0.0.0.0",
