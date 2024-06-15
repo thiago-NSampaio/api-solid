011446a17721660324c960a9ebee9beee62941af
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-06-15 16:07:12 -0400

    Tests RBAC

--- src/http/controllers/check-ins.ts/history.spec.ts
+++ src/http/controllers/check-ins.ts/history.spec.ts
@@ -14,7 +14,7 @@ describe('Check-In History (e2e)', () => {
   })
 
   it('should be able to list the history of check-ins', async () => {
-    const {token} = await createAndAuthenticateUser(app)
+    const {token} = await createAndAuthenticateUser(app, true)
 
     const user = await prisma.user.findFirstOrThrow()
 
--- src/http/controllers/check-ins.ts/metrics.spec.ts
+++ src/http/controllers/check-ins.ts/metrics.spec.ts
@@ -14,7 +14,7 @@ describe('Check-In Metrics (e2e)', () => {
   })
 
   it('should be able to get the total count of check-ins', async () => {
-    const {token} = await createAndAuthenticateUser(app)
+    const {token} = await createAndAuthenticateUser(app, true)
 
     const user = await prisma.user.findFirstOrThrow()
 
